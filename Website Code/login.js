7decb530e3e7ea4a9375475c88d1225f3b79a681
c-a-e-z-a-r <152083162+c-a-e-z-a-r@users.noreply.github.com>
2024-04-09 15:50:31 +0800

    login.js

--- Website Code/login.js
+++ Website Code/login.js
@@ -1,7 +1,7 @@
 function checkFields() {
   let username = document.getElementById("username").value.trim();
   let password = document.getElementById("password").value.trim();
-  let message = document.getElementById("message").value.trim();
+  let message = document.getElementById("message");
   if (username === "" || password === "") {
     message.textContemt = "Please fill all fields";
   } else {
